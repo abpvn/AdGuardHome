0389e5584754c8c58f3f97cca1fcbfe831750056
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-06-18 08:10:45 +0700

    Bump base version up

--- build-dev.sh
+++ build-dev.sh
@@ -3,7 +3,7 @@ if [ "$VERSION" = '' ]
 then
     if [ "$BASE_VERSION" = '' ]
     then
-        BASE_VERSION='v0.108.3'
+        BASE_VERSION='v0.108.4'
         readonly BASE_VERSION
     fi
     commit_number="$( git rev-list --count master..HEAD )"

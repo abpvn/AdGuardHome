ed0282d17432dab6638e7299cfa2f732595989a7
Hoàng Rio <donghoang.nguyen@outlook.com>
2025-06-06 13:39:31 +0700

    D: useless shell script

--- copy-to-srv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rsync -hav AdGuardHome root@srv.abpvn.com:/var/www/html/dns/AdGuardHome_dev
-ssh root@srv.abpvn.com "./update-dns.sh"
-rsync -hav AdGuardHome root@srv2.abpvn.com:/var/www/html/dns/AdGuardHome_dev
-ssh root@srv2.abpvn.com "./update-dns.sh"
